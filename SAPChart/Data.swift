cf863c6e7df05d440881fdfaae4ec4edab4488ff
Jacopo Mangiavacchi <Jacopo.Mangiavacchi@uk.ibm.com>
2017-04-12 15:23:27 +0100

    Added Horizontal bar on main view

--- SAPChart/Data.swift
+++ SAPChart/Data.swift
@@ -10,6 +10,8 @@ import Foundation
 
 struct Data {
     static let plantStatus = ["PP" : 80, "FP" : 15, "CP" : 45, "UFP" : 87, "SA" : 80]
+    static let groupStatus = ["Month" : 20, "6Months" : 15, "Avg1Y" : 45, "FULL" : 65]
+
 }
 
 
---
+++ SAPChart/MonthEndViewController.swift
@@ -0,0 +1,175 @@
+//
+//  MonthEndViewController.swift
+//  SAPChart
+//
+//  Created by Jacopo Mangiavacchi on 4/10/17.
+//  Copyright © 2017 Jacopo. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class MonthEndViewController: UIViewController, ChartViewDelegate {
+
+    @IBOutlet weak var ppChart: PieChartView!
+    @IBOutlet weak var fpChart: PieChartView!
+    @IBOutlet weak var cpChart: PieChartView!
+    @IBOutlet weak var ufpChart: PieChartView!
+    @IBOutlet weak var saChart: PieChartView!
+    
+    @IBOutlet weak var groupAccountingChart: HorizontalBarChartView!
+    
+    @IBOutlet weak var publishButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configPieChart(ppChart, label: "PP")
+        configPieChart(fpChart, label: "FP")
+        configPieChart(cpChart, label: "CP")
+        configPieChart(ufpChart, label: "UFP")
+        configPieChart(saChart, label: "SA")
+
+        configBarChart(groupAccountingChart)
+        
+        updatePieChartWithData(ppChart, value: Data.plantStatus["PP"]!, label: "PP")
+        updatePieChartWithData(fpChart, value: Data.plantStatus["FP"]!, label: "FP")
+        updatePieChartWithData(cpChart, value: Data.plantStatus["CP"]!, label: "CP")
+        updatePieChartWithData(ufpChart, value: Data.plantStatus["UFP"]!, label: "UFP")
+        updatePieChartWithData(saChart, value: Data.plantStatus["SA"]!, label: "SA")
+
+        updateBarChartWithData(groupAccountingChart, value: Data.groupStatus["Month"]!)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        ppChart.animate(xAxisDuration: Constants.animationTime, easingOption: .easeOutBack)
+        fpChart.animate(xAxisDuration: Constants.animationTime, easingOption: .easeOutBack)
+        cpChart.animate(xAxisDuration: Constants.animationTime, easingOption: .easeOutBack)
+        ufpChart.animate(xAxisDuration: Constants.animationTime, easingOption: .easeOutBack)
+        saChart.animate(xAxisDuration: Constants.animationTime, easingOption: .easeOutBack)
+        
+        groupAccountingChart.animate(yAxisDuration: Constants.animationTime, easingOption: .easeOutBack)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    internal func configPieChart(_ pieChartView: PieChartView, label: String) {
+        pieChartView.delegate = self
+        
+        pieChartView.entryLabelColor = UIColor.white
+        pieChartView.entryLabelFont = UIFont(name: "HelveticaNeue-Light", size: 12.0)
+
+        pieChartView.usePercentValuesEnabled = true
+        pieChartView.drawSlicesUnderHoleEnabled = false
+        pieChartView.holeRadiusPercent = 0.58
+        pieChartView.transparentCircleRadiusPercent = 0.61
+        pieChartView.setExtraOffsets(left: 5.0, top: 10.0, right: 5.0, bottom: 5.0)
+        pieChartView.drawHoleEnabled = true
+        pieChartView.rotationAngle = 0.0
+        pieChartView.rotationEnabled = true
+        pieChartView.highlightPerTapEnabled = true
+        
+        pieChartView.chartDescription?.text = nil
+    
+        pieChartView.drawCenterTextEnabled = true
+        pieChartView.centerText = label
+    
+        pieChartView.legend.setCustom(entries: [])
+    }
+
+    
+    internal func configBarChart(_ HorizontalBarChartView: HorizontalBarChartView) {
+        HorizontalBarChartView.delegate = self
+        
+        HorizontalBarChartView.highlightPerTapEnabled = false
+        
+        HorizontalBarChartView.chartDescription?.text = nil
+        HorizontalBarChartView.legend.setCustom(entries: [])
+        
+        
+        HorizontalBarChartView.drawGridBackgroundEnabled = false
+        HorizontalBarChartView.dragEnabled = false
+        HorizontalBarChartView.setScaleEnabled(false)
+        HorizontalBarChartView.pinchZoomEnabled = false
+        HorizontalBarChartView.xAxis.labelPosition = .bottom
+        HorizontalBarChartView.xAxis.labelFont = UIFont.systemFont(ofSize: 10, weight: UIFontWeightLight)
+        HorizontalBarChartView.xAxis.drawAxisLineEnabled = true
+        HorizontalBarChartView.xAxis.drawGridLinesEnabled = false
+        HorizontalBarChartView.xAxis.granularity = 10.0
+
+        HorizontalBarChartView.drawBarShadowEnabled = false
+        HorizontalBarChartView.drawValueAboveBarEnabled = false
+        HorizontalBarChartView.maxVisibleCount = 60;
+        
+        HorizontalBarChartView.leftAxis.labelFont = UIFont.systemFont(ofSize: 10, weight: UIFontWeightLight)
+        HorizontalBarChartView.leftAxis.drawAxisLineEnabled = true
+        HorizontalBarChartView.leftAxis.drawGridLinesEnabled = true
+        HorizontalBarChartView.leftAxis.axisMinimum = 0.0 // this replaces startAtZero = YES
+        
+        HorizontalBarChartView.rightAxis.enabled = true
+        HorizontalBarChartView.rightAxis.labelFont = UIFont.systemFont(ofSize: 10, weight: UIFontWeightLight)
+        HorizontalBarChartView.rightAxis.drawAxisLineEnabled = true
+        HorizontalBarChartView.rightAxis.drawGridLinesEnabled = false
+        HorizontalBarChartView.rightAxis.axisMinimum = 0.0 // this replaces startAtZero = YES
+        
+        HorizontalBarChartView.fitBars = true
+    }
+
+    
+    internal func updatePieChartWithData(_ pieChartView: PieChartView, value: Int, label: String) {
+        let dataEntries = [PieChartDataEntry(value: Double(value), label: "\(value)%"), PieChartDataEntry(value: Double(100-value), label: "\(100-value)%")]
+        let chartDataSet = PieChartDataSet(values: dataEntries, label: label)
+        
+        chartDataSet.sliceSpace = 2.0
+        chartDataSet.colors = [Constants.darkColor, Constants.lightColor]
+        chartDataSet.drawValuesEnabled = false
+        
+        let chartData = PieChartData(dataSet: chartDataSet)
+        pieChartView.data = chartData
+    }
+    
+
+    internal func updateBarChartWithData(_ HorizontalBarChartView: HorizontalBarChartView, value: Int) {
+        let dataEntries = [BarChartDataEntry(x: 0.0, yValues: [Double(value), Double(100-value)])]
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: nil)
+        
+        chartDataSet.colors = [Constants.darkColor, Constants.lightColor]
+        chartDataSet.drawValuesEnabled = false
+        
+        let chartData = BarChartData(dataSet: chartDataSet)
+        HorizontalBarChartView.data = chartData
+    }
+    
+
+    @IBAction func onMonthGroup(_ sender: Any) {
+    }
+    
+    @IBAction func onSixMonthGroup(_ sender: Any) {
+    }
+    
+    @IBAction func onAvgOneYearGroup(_ sender: Any) {
+    }
+    
+    @IBAction func onFullGroup(_ sender: Any) {
+    }
+    
+    @IBAction func onPublish(_ sender: Any) {
+    }
+    
+    @IBAction func onDiagnostics(_ sender: Any) {
+    }
+
+    
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        print("selected")
+    }
+    
+    func chartValueNothingSelected(_ chartView: ChartViewBase) {
+        print("unselected")
+    }
+    
+
+}

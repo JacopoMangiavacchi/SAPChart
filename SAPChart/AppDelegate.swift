9e2eed2da1999e4ca1e57b491ed4653de2f4a96c
Jacopo Mangiavacchi <Jacopo.Mangiavacchi@uk.ibm.com>
2017-04-20 11:58:12 +0100

    Removed NavigationBar 1 pixel border

--- SAPChart/AppDelegate.swift
+++ SAPChart/AppDelegate.swift
@@ -19,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         UIApplication.shared.statusBarStyle = .lightContent
         
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
+        
+        
         if let path = Bundle.main.path(forResource: "data", ofType: "json") {
             do {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)

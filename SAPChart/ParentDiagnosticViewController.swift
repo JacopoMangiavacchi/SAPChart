25a87cea2044c7ac7c117c402399e213e95e3a5e
Jacopo Mangiavacchi <Jacopo.Mangiavacchi@uk.ibm.com>
2017-04-24 17:42:10 +0100

    Adding completition on diagnostics view .. work in progress

---
+++ SAPChart/ParentDiagnosticViewController.swift
@@ -0,0 +1,137 @@
+//
+//  DiagnosticViewController.swift
+//  SAPChart
+//
+//  Created by Jacopo Mangiavacchi on 4/24/17.
+//  Copyright © 2017 Jacopo. All rights reserved.
+//
+
+import UIKit
+import Charts
+import SwiftyJSON
+
+
+class DiagnosticViewController: UIViewController {
+
+    var diagnosticsView: DiagnosticView!
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        configTicketLineChart(diagnosticsView.ticketsLineChartView)
+        configCompletitionLineChart(diagnosticsView.completitionLineChartView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        updateTicketLineChart(diagnosticsView.ticketsLineChartView)
+        updateCompletitionLineChart(diagnosticsView.completitionLineChartView)
+    }
+
+    
+    func configTicketLineChart(_ lineChartView: LineChartView) {
+        lineChartView.highlightPerTapEnabled = false
+        
+        lineChartView.chartDescription?.enabled = false
+        lineChartView.chartDescription?.text = nil
+        lineChartView.legend.enabled = false
+        lineChartView.legend.setCustom(entries: [])
+        
+        
+        lineChartView.drawGridBackgroundEnabled = false
+        lineChartView.dragEnabled = false
+        lineChartView.setScaleEnabled(true)
+        lineChartView.pinchZoomEnabled = false
+        
+        //lineChartView.setViewPortOffsets(left: 10.0, top: 0.0, right: 10.0, bottom: 0.0)
+        
+        lineChartView.leftAxis.enabled = false
+        lineChartView.leftAxis.spaceTop = 0.4
+        lineChartView.leftAxis.spaceBottom = 0.4
+        lineChartView.rightAxis.enabled = false
+        lineChartView.xAxis.enabled = false
+    }
+    
+    
+    func configCompletitionLineChart(_ lineChartView: LineChartView) {
+        lineChartView.highlightPerTapEnabled = false
+        
+        lineChartView.chartDescription?.enabled = false
+        lineChartView.chartDescription?.text = nil
+        lineChartView.legend.enabled = false
+        lineChartView.legend.setCustom(entries: [])
+        
+        
+        lineChartView.drawGridBackgroundEnabled = false
+        lineChartView.dragEnabled = false
+        lineChartView.setScaleEnabled(true)
+        lineChartView.pinchZoomEnabled = false
+        
+        //lineChartView.setViewPortOffsets(left: 10.0, top: 0.0, right: 10.0, bottom: 0.0)
+        
+        lineChartView.leftAxis.enabled = false
+        lineChartView.leftAxis.spaceTop = 0.4
+        lineChartView.leftAxis.spaceBottom = 0.4
+        lineChartView.rightAxis.enabled = false
+        lineChartView.xAxis.enabled = false
+    }
+    
+    
+
+    func updateTicketLineChart(_ lineChartView: LineChartView) {
+        func setDataSet(_ chartDataSet: LineChartDataSet, color: UIColor) {
+            chartDataSet.lineWidth = 1.75
+            chartDataSet.circleRadius = 10.0
+            chartDataSet.circleHoleRadius = 8
+            chartDataSet.setColor(color)
+            chartDataSet.setCircleColor(color)
+            chartDataSet.circleHoleColor = UIColor.white
+            
+            //chartDataSet.colors = [Constants.darkColor, Constants.lightColor]
+            chartDataSet.drawValuesEnabled = false
+        }
+        
+        let dataEntries1 = [ChartDataEntry(x: 0.0, y: 3.0), ChartDataEntry(x: 1.0, y: 1.0), ChartDataEntry(x: 2.0, y: 7.0), ChartDataEntry(x: 3.0, y: 3.0), ChartDataEntry(x: 4.0, y: 1.0), ChartDataEntry(x: 5.0, y: 7.0)]
+        let dataEntries2 = [ChartDataEntry(x: 0.0, y: 5.0), ChartDataEntry(x: 1.0, y: 13.0), ChartDataEntry(x: 2.0, y: 5.0), ChartDataEntry(x: 3.0, y: 10.0), ChartDataEntry(x: 4.0, y: 7.0), ChartDataEntry(x: 5.0, y: 11.0)]
+        
+        
+        let chartDataSet1 = LineChartDataSet(values: dataEntries1, label: "")
+        let chartDataSet2 = LineChartDataSet(values: dataEntries2, label: "")
+        
+        setDataSet(chartDataSet1, color:Constants.darkColor)
+        setDataSet(chartDataSet2, color:Constants.lightColor)
+        
+        let chartData = LineChartData(dataSets: [chartDataSet1, chartDataSet2])
+        lineChartView.data = chartData
+    }
+
+    
+    func updateCompletitionLineChart(_ lineChartView: LineChartView) {
+        func setDataSet(_ chartDataSet: LineChartDataSet, color: UIColor) {
+            chartDataSet.lineWidth = 1.75
+            chartDataSet.drawCirclesEnabled = false
+            chartDataSet.setColor(color)
+            
+            chartDataSet.fillAlpha = 0.26
+            chartDataSet.fillColor = Constants.darkColor
+            chartDataSet.drawFilledEnabled = true
+            
+            //chartDataSet.colors = [Constants.darkColor, Constants.lightColor]
+            chartDataSet.drawValuesEnabled = false
+        }
+        
+        let dataEntries1 = [ChartDataEntry(x: 0.0, y: 10.0), ChartDataEntry(x: 1.0, y: 26.0), ChartDataEntry(x: 2.0, y: 10.0), ChartDataEntry(x: 3.0, y: 20.0), ChartDataEntry(x: 4.0, y: 14.0), ChartDataEntry(x: 5.0, y: 22.0)]
+        let dataEntries2 = [ChartDataEntry(x: 0.0, y: 5.0), ChartDataEntry(x: 1.0, y: 13.0), ChartDataEntry(x: 2.0, y: 5.0), ChartDataEntry(x: 3.0, y: 10.0), ChartDataEntry(x: 4.0, y: 7.0), ChartDataEntry(x: 5.0, y: 11.0)]
+        
+        
+        let chartDataSet1 = LineChartDataSet(values: dataEntries1, label: "")
+        let chartDataSet2 = LineChartDataSet(values: dataEntries2, label: "")
+        
+        setDataSet(chartDataSet1, color:Constants.darkColor)
+        setDataSet(chartDataSet2, color:Constants.lightColor)
+        
+        let chartData = LineChartData(dataSets: [chartDataSet1, chartDataSet2])
+        lineChartView.data = chartData
+    }
+}
